c55ea35d5954beb098a6a08daa4f36ca7a674db5
dantvi <daniel.tving@gmail.com>
2024-07-14 19:29:53 +0200

    load 10 photos

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const saveConfirmed = document.querySelector('.save-confirmed');
 const loader = document.querySelector('.loader');
 
 // NASA API
-const count = 1;
+const count = 10;
 const apiKey = 'CHr5FArb60YjjldYZGTln8YVfPwOm4hptQzfwRHI';
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
